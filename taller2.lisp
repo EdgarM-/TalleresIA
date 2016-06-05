2862498bc8b0ef937290a8e67313c1b26bd7a1f2
Edgar M. Amezquita <ed.amezqui.95@gmail.com>
2016-06-05 14:57:03 -0700

    Taller 2

--- taller2.lisp
+++ taller2.lisp
@@ -7,12 +7,15 @@
 ; 
 ;Funcion principal donde llama las funciones necesarias para poder pasar la formula a FNC
 (defun main(exp)
-	(disAndOr (applyNeg (resolveImp (resolveDoubleI (resolveEx (resolveFA exp () ))))))
+	(disAndOr (applyNeg (resolveImp (resolveDoubleI (resolveEx (resolveFA exp ))))))
 )
+
+(defun resolveFA(exp)
+	(newresolveFA exp nil nil nil)
+	)
 ;Resuelve el para todo haciendo el renombramiento de variable cuando es necesario, revisando las variable que se usaron previamente 
-(defun resolveFA(exp vars)
-	(cond ((null exp) exp)
-		((equal (car exp) 'forall) (resolveFA (removeOp exp ()) (addvars (cdar exp) vars)))
+(defun newresolveFA(exp var reemp vars)
+	(cond ((equal (car exp) forall) (append ))
 		)	
 	)
 ;Revisa si existe un elemento en una lista
@@ -32,27 +35,35 @@
 	)
 ;Resuelve la implicacion cambiandola por no p o q.
 (defun resolveImp (exp)
-	(cond ((equal (car exp) 'imp) (cons 'or (list (list 'not  (resolveImp (cadr exp))) (resolveImp (caddr exp)))) )
-		((not (in (car exp) '(imp or and not))) (append exp))
-		(t (append (list (car exp) (list (resolveImp (cadr exp)) (resolveImp (caddr exp))))))
+	(cond ((equal (car exp) 'imp) (append (list 'or) (list (list 'not  (resolveImp (cadr exp)))) (resolveImp (caddr exp))))
+		((not (in (car exp) '(or and not))) (cons exp))
+		(t (append (list (car exp) (resolveImp (cadr exp)) (resolveImp (caddr exp)))))
 		)
 	)
 
-;Aplica la negacion donde esta se encuentre y la distribuye
-(defun applyNeg (exp flag)
-	
+;Llama la funcion auxiliar con los parametros iniciales para calcular la negacion 
+(defun applyNeg (exp)
+	(newapplyNeg exp 0)
 )
+;Aplica la negacion donde esta se encuentre y la distribuye
 
-(defun newapplyNeg (exp)
-	(cond ((equal (car exp) 'not) (append (newapplyNeg (cadr exp))))
-		((not (in (car exp) '(or and not))) (append exp))
-		(t (append (list (car exp) (newapplyNeg (cadr exp)))))
+(defun newapplyNeg (exp flag)
+	(cond ((and (equal (car exp) 'not) (equal flag '0)) (newapplyNeg (cadr exp) '1))
+		((and (equal (car exp) 'not) (equal flag '1)) (newapplyNeg  (cadr exp) '0))
+		((and (not (in (car exp) '(or and))) (equal flag '0)) (append exp))
+		((and (not (in (car exp) '(or and))) (equal flag '1)) (append (list 'not exp)))
+		((and (equal (car exp) 'and) (equal flag '1)) (append (list 'or (newapplyNeg (cadr exp) '1) (newapplyNeg (caddr exp) '1))))
+		((and (equal (car exp) 'or) (equal flag '1)) (append (list 'and (newapplyNeg (cadr exp) '1) (newapplyNeg (caddr exp) '1))))
+		(t (append (list (car exp) (newapplyNeg (cadr exp) '0) (newapplyNeg (caddr exp) '0))))
 
 		)
-	)
-;Devuelve el largo de la lista l dadas
-(defun len (l n)
-	(cond ((null l) n)
-		(t (len (cdr l) (+ 1 n)))
-	)
-	)
+	)	
+
+;Llama a la funcion newdisAndOr con lo parametros iniciales
+(defun disAndOr (exp)
+	;(newdisAndOr exp 0 0 )
+)
+;Distribuye and/or
+;(defun newdisAndOr (exp andF orF)
+
+;	)
